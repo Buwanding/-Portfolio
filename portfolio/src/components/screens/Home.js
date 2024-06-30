d9ab7306663ce033bad09ef8d59e687c843f3236
Buwanding <buwandingaladdin@gmail.com>
2024-06-30 22:55:34 +0800

    home, navagation and footer

---
+++ portfolio/src/components/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navpart from '../parts/Navpart'
+import Footerpart from '../parts/Footerpart'
+
+export const Home = () => {
+  return (
+    <div>
+        <Navpart/>
+        <Footerpart/>
+    </div>
+  )
+}
